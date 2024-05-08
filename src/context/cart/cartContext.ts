aeabb6cb2fae43b2cc8902e6f409987dcf9232e6
divyanshu <divyanshulohar2002@gmail.com>
2024-05-08 21:42:06 +0530

    some changes

--- src/context/cart/cartContext.ts
+++ src/context/cart/cartContext.ts
@@ -1,6 +1,17 @@
-"use client"
-import { createContext } from "react";
+import { MenuItem } from '@/app/restaurants/[id]/page'
+import { Dispatch, SetStateAction, createContext, useContext, useState } from 'react'
 
-const employeeContext = createContext();
+export const defaultCartState: MenuItem[] = []
 
-export default employeeContext;
+interface CartContextType {
+  cartState: MenuItem[];
+  setCartState: Dispatch<SetStateAction<MenuItem[]>>;
+}
+
+export const CartStateContext = createContext<CartContextType>({
+  cartState: defaultCartState,
+  setCartState: () => {}
+})
+
+export const useCartState = () => useContext(CartStateContext)
+
