803524a6c469ad447f449ec9026caee6851f031b
divyanshu <divyanshulohar2002@gmail.com>
2024-05-08 11:56:00 +0530

    some changes

--- src/components/Restaurant/Customization.tsx
+++ src/components/Restaurant/Customization.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import veg from "@/Images/veg-icon.svg";
+import { IoMdClose } from 'react-icons/io';
 
 const Customization = ({ setCount, count }: { setCount: React.Dispatch<React.SetStateAction<number>>; count: number }) => {
   const [totalAmount, setTotalAmount] = useState(139.00);
@@ -24,11 +25,18 @@ const Customization = ({ setCount, count }: { setCount: React.Dispatch<React.Set
   };
 
   return (
-    <div className={`fixed bottom-0 left-0 z-10 w-full bg-white rounded-t-3xl duration-300 transform ${isOpen ? 'translate-y-0' : '-translate-y-full'}`}>
+    <div className={`fixed bottom-0 left-0 z-10 w-full bg-white rounded-t-3xl h-[60%] duration-500 flex flex-col justify-between transform transition-all ${isOpen ? 'translate-y-0' : 'translate-y-full'}`}>
       <div className="p-4">
-        <div className="flex items-center">
-          <Image alt='veg' src={veg} width={23} height={23} />
-          <h3 className="pl-2 text-lg">Americano</h3>
+        <div className="flex items-center justify-between">
+          <div className='flex '>
+            <Image alt='veg' src={veg} width={23} height={23} />
+            <h3 className="pl-2 text-lg">Americano</h3>
+          </div>
+          <div className='pr-5'>
+            <IoMdClose onClick={()=>{
+              setCount(0)
+            }} size={25} />
+          </div>
         </div>
         <hr className="border-gray-300 border-0.5 my-4" />
         <div className="space-y-4 text-sm text-gray-500">
---
+++ src/context/cart/cartContext.ts
@@ -0,0 +1,6 @@
+"use client"
+import { createContext } from "react";
+
+const employeeContext = createContext();
+
+export default employeeContext;
